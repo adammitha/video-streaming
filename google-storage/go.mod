d68e005c347c18596dadf0e803c60bfebb24b438
Adam Mitha <adam.mitha@gmail.com>
2021-02-27 21:10:55 -0800

    GCS service

--- google-storage/go.mod
+++ google-storage/go.mod
@@ -1,3 +1,11 @@
 module github.com/adammitha/video-streaming/google-storage
 
 go 1.16
+
+require (
+	cloud.google.com/go/storage v1.13.0
+	github.com/adammitha/video-streaming/utils v0.0.0-00010101000000-000000000000
+	github.com/gorilla/mux v1.8.0
+)
+
+replace github.com/adammitha/video-streaming/utils => ../utils
--- google-storage/main.go
+++ google-storage/main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"net/http"
+
+	"cloud.google.com/go/storage"
+	"github.com/adammitha/video-streaming/utils"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/{path}", videoHandler)
+	r.Use(utils.LoggingMiddleware)
+
+	log.Printf("Listening on port 8081")
+	log.Fatal(http.ListenAndServe(":8081", r))
+}
+
+func videoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer client.Close()
+
+	rc, err := client.Bucket("video-storage-306005").Object(vars["path"]).NewReader(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rc.Close()
+	io.Copy(w, rc)
+
+}
